4186e05fd90a9de1f231afb3393d1f36fb59838d
Yevhenii Kushnir <aroblack17@gmail.com>
2024-11-16 19:03:11 +0100

    make build

---
+++ index.282ed473.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target;r&&("IMG"===r.tagName?e.src=r.parentElement.href:"A"===r.tagName&&(e.src=r.href))});
+//# sourceMappingURL=index.282ed473.js.map
